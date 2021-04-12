cb5aad66163388e85ed739e1b5e3e3f64f282720
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2021-04-12 16:45:05 -0600

    [UPDATE] Updated App JS

--- front-end-code/src/App.js
+++ front-end-code/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Document Analyzer
+          Document Analyzer Main Page
         </p>
       </header>
     </div>

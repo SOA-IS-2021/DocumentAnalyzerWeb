dbadd73e0accf1dcea67083e6eeeb01112072fba
Fabricio Elizondo Fernández <fabelifer@gmail.com>
2021-04-14 15:40:46 -0600

    [UPDATE] Login integration with the documents table

--- front-end-code/src/Home.js
+++ front-end-code/src/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Table from "./components/Table"
+import UploadFiles from "./components/UploadFiles"
 
 const Home = ({handleLogOut}) => {
     return(
@@ -7,7 +9,52 @@ const Home = ({handleLogOut}) => {
                 <h2>Welcome to the Document Analyzer Web Page</h2>
                 <button onClick = {handleLogOut}>Logout</button>
             </nav>
+            <div>
+            <section id="split">          
+        <div className="ui two column stackable center aligned grid">    
+
+          <div className="column">                        
+            <div className="ui placeholder segment">
+             <UploadFiles />       
+            </div>
+
+            <div class="ui placeholder segment">
+              <div class="ui icon header">
+                <i class="file outline icon"></i>
+                <h3>Search for employees</h3> 
+              </div>
+              <div class="ui primary button">Select Document</div>
+            </div>
+
+            <div className="ui raised padded text container segment">
+              <Table
+                query={{
+                  documents: [
+                  { file: 'gg.pdf', employee: 'Manuel'},
+                  { file: 'hh.docx', employee: 'Juan'} ],
+                  mode : 'Search Results'}} 
+              />
+            </div>
+          </div>
+                      
+          <div className="column">
+            <div className="ui raised padded text container segment">
+              <Table
+               query={{
+                documents: [
+                 { file: 'gg.pdf', progress: 5},
+                 { file: 'hh.docx', progress: 96},
+                 { file: 'readme.txt', progress: 78},
+                 { file: 'hola.txt', progress: 45} ],
+                mode : 'History'}} 
+              />
+            </div>                      
+          </div>
+        </div>
+        </section> 
+            </div>
         </section>
+
     )
 };
 
